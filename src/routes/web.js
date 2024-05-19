48e9b6ea8b8e87ec132affc3cf25c0245fa71569
Rynare <rynare.of.legend@gmail.com>
2024-05-19 11:14:07 +0700

    <update>[route] menambahkan rute baru untuk web

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/named */
-import { HomeController } from "../app/controller/HomeController.js";
+// import { HomeController } from "../app/controller/HomeController.js";
+import { LoginController } from "../app/controller/LoginController.js";
 import { renderView } from "../utils/helper/view-helper.js";
 import { router } from "./router.js";
 
@@ -7,7 +8,7 @@ const routes = [
   router(404, () => renderView("/pages/404.html")),
   router("/", () => renderView("/pages/home.html")),
   router("/home", () => renderView("/pages/home.html")),
-  router("/example", [new HomeController(), "index"]),
+  router("/example", [new LoginController(), "index"]),
 ];
 
 export { routes };
