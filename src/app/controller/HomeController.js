7ee6aeb4bdeda43ef659f78d81c1860f1e054a77
Rynare <rynare.of.legend@gmail.com>
2024-06-02 07:29:09 +0700

    <update> [home] menerapkan api dan memberikan fungsionalitas baru untuk home page

--- src/app/controller/HomeController.js
+++ src/app/controller/HomeController.js
@@ -1,3 +1,4 @@
+import $ from "jquery";
 import { Controller } from "./Controller.js";
 
 class HomeController extends Controller {
@@ -19,14 +20,38 @@ class HomeController extends Controller {
     const articleSlider = document.querySelector(".popular-articles");
     for (let amount = 1; amount <= 10; amount++) {
       articleSlider.innerHTML += `
-        <div class="card placeholder-wave">
+        <div class="card placeholder-glow  bg-white rounded">
             <div class="placeholder" style="aspect-ratio: 3/2; width: 30vw;max-width: 220px;"></div>
         </div>`;
       recipeSlider.innerHTML += `
-        <div class="card placeholder-wave">
+        <div class="card placeholder-glow bg-white rounded">
             <div class="placeholder" style="aspect-ratio: 3/2; width: 30vw;max-width: 220px;"></div>
         </div>`;
     }
+
+    $.get(`${process.env.API_ENDPOINT}/api/popular-recipes`).done((response) => {
+      recipeSlider.innerHTML = "";
+      const { results } = response;
+      results.forEach((result) => {
+        const resultStr = JSON.stringify(result);
+        const topRecipeCard = document.createElement("div", { is: "recipe-card" });
+        topRecipeCard.classList.add(..."recipe-card position-relative rounded bg-white text-black".split(" "));
+        topRecipeCard.setAttribute("json-data", resultStr);
+        recipeSlider.appendChild(topRecipeCard);
+      });
+    });
+
+    $.get(`${process.env.API_ENDPOINT}/api/popular-articles`).done((response) => {
+      articleSlider.innerHTML = "";
+      const { results } = response;
+      results.forEach((result) => {
+        const resultStr = JSON.stringify(result);
+        const topArticleCard = document.createElement("div", { is: "article-card" });
+        topArticleCard.classList.add(..."article-card overflow-hidden rounded".split(" "));
+        topArticleCard.setAttribute("json-data", resultStr);
+        articleSlider.appendChild(topArticleCard);
+      });
+    });
   }
 }
 
