4fa29aeb81b3c07c9ca02da967e3c639ef922aac
Rynare <rynare.of.legend@gmail.com>
2024-06-03 17:26:30 +0700

    <update> [anchor|route] mengubah penamaan route dan anchor

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,7 @@
 /* eslint-disable import/named */
 import { HomeController } from "../app/controller/HomeController.js";
 import { KulinerController } from "../app/controller/KulinerController.js";
-import { LoginController } from "../app/controller/LoginController.js";
 import { ResepController } from "../app/controller/ResepController.js";
-import { TestQuillController } from "../app/controller/TestQuill.js";
 import { renderView } from "../utils/helper/view-helper.js";
 import { router } from "./router.js";
 
@@ -15,8 +13,6 @@ const routes = [
   router("/artikel-detail/:id", [new KulinerController(), "detail"]),
   router("/resep", [new ResepController(), "index"]),
   router("/resep-detail/:id", [new ResepController(), "index"]),
-  router("/example", [new LoginController(), "index"]),
-  router("/quill", [new TestQuillController(), "index"]),
 ];
 
 export { routes };
